d233bdc2a3f3d8b2cbb8739fea5f46b59e45fd02
Mihir-gif <200522598@student.georgianc.on.ca>
2024-03-29 16:27:31 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tutorial-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
