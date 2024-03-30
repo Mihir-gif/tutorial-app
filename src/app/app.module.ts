7a70c860b0845d75236d216b6b12ff58702cfca8
Mihir-gif <200522598@student.georgianc.on.ca>
2024-03-30 12:31:19 -0400

    Enabled hash routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // Add this line
+  ],  bootstrap: [AppComponent],
 })
 export class AppModule {}
